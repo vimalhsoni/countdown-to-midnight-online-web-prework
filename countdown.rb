82c6c991fe7cca6bf0d884253ea828acceeacec9
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-03 22:31:06 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
-
-def countdown
+def countdown(num)
+x = num
+while x > 0
+  puts "#{x} SECOND(S)!"
+  x -= 1
 end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  x = num
+  while x >= 0
+    sleep 1
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+    return "HAPPY NEW YEAR!"
+  end
