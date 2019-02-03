b96d403b2623782a56c17f4b4ff7b5d65ebfc981
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-03 22:25:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,18 @@
 def countdown(num)
 x = num
-while x > 0
+while x >= 0
   puts "#{x} SECOND(S)!"
   x -= 1
 end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(num)
   x = num
   while x >= 0
     sleep 1
-    "#{x} SECOND(S)!"
+    puts "#{x} SECOND(S)!"
     x -= 1
   end
-    puts "HAPPY NEW YEAR!"
+    return "HAPPY NEW YEAR!"
   end
